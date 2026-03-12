a0b14df9199898a7389d7e4f17ea1d6c00502c57
Boris Britva <boris_britva@gmail.com>
2026-03-12 21:50:30 +0800

    12.03.2026

--- modules/networks/main.tf
+++ modules/networks/main.tf
@@ -9,4 +9,20 @@ resource "yandex_vpc_subnet" "main" {
   zone           = var.zone
   network_id     = yandex_vpc_network.main.id
   v4_cidr_blocks = [var.subnet_cidr]
+
+  route_table_id = yandex_vpc_route_table.nat.id
+}
+
+resource "yandex_vpc_gateway" "nat" {
+  name = "nat-gateway"
+  shared_egress_gateway {}
+}
+
+resource "yandex_vpc_route_table" "nat" {
+  network_id = yandex_vpc_network.main.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    gateway_id         = yandex_vpc_gateway.nat.id
+  }
 }
